83f1a037288adb6f15ba40ff8fdf3ff5c610c1da
Димитър Димитров <dimitar.add@gmail.com>
2026-03-04 08:13:57 +0200

    modified:   pages/_app.js 	new file:   public/favicon-192.png 	new file:   public/favicon-32.png 	new file:   public/favicon.ico

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import { useRouter } from "next/router";
+import Head from "next/head";
 import FallingElements from "../components/FallingElements";
 
 export default function App({ Component, pageProps }) {
@@ -7,6 +8,12 @@ export default function App({ Component, pageProps }) {
   const isAdmin = router.pathname.startsWith("/admin");
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-32.png" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/favicon-192.png" />
+        <meta name="theme-color" content="#f8b7d1" />
+      </Head>
       {!isAdmin && <FallingElements />}
       <Component {...pageProps} />
     </>
